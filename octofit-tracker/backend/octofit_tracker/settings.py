828426238903abf91354dbda927176f3842a69e9
lubos-zavazan <lubos.zavazan@accenture.com>
2025-12-25 12:40:25 +0000

    commit 3

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-%(-v5$k2d5-6d$zx51_x(*kou%9521neg3rlkh4*2l@%#ssx=t
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
